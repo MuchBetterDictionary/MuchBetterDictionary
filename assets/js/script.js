d784974d6dd40b3bc4838f8c125326d731ff29ab
Jessica Chen <jessicachen28@gmail.com>
2022-04-12 22:41:56 -0400

    Implemented Local Storage, Past Search History Display, Ability to Search by Past History, Modularized Logic, and Updated Word of the Day to Random

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Declare Global Variables
-//Public API Keys
+// Public API Keys
 let merriamApiKey = "676327db-9f8e-4212-a0e1-d73850d216df";
 let pexelApiKey = "563492ad6f917000010000014a4078a4f8b545fda3f3c33d260ab9d0";
 
@@ -7,10 +7,125 @@ let pexelApiKey = "563492ad6f917000010000014a4078a4f8b545fda3f3c33d260ab9d0";
 let mainEl = document.querySelector("#main-content");
 let mainCard = document.querySelector("#card-main");
 let definitionCard = document.querySelector("#card-definitions");
+let pastSearch = document.querySelector("#past-searches");
 let searchWord = document.querySelector("#word-search-form");
 let randomWord = document.querySelector("#random-word");
+let resetSearch = document.querySelector("#clear-searches");
 
-// Function to Display Definitions by Part of Speech
+// Function to Handle Events for Past Searches
+function pastSearchHandler(event) {
+  var word = event.target.getAttribute("data-word");
+
+  // Reset Main Section
+  mainCard.innerHTML = "";
+  definitionCard.innerHTML = "";
+
+  if (word) {
+    dictProcessor(word);
+  }
+}
+
+// Function to Display Search History On Load
+function displaySearchHistory() {
+  // Get History from Local Storage
+  var history = localStorage.getItem("searches");
+
+  // Parse and Display History, if available
+  if (history) {
+    pastSearch.classList.remove("hidden");
+    history = JSON.parse(history);
+
+    for (var i = 0; i < history.length; i++) {
+      var searchEntry = document.createElement("button");
+      searchEntry.textContent = history[i].word;
+      searchEntry.classList.add("search-entry");
+      searchEntry.setAttribute("data-word", history[i].word);
+      searchEntry.setAttribute("type", "submit");
+
+      pastSearch.appendChild(searchEntry);
+    }
+  }
+}
+
+// Function to Store Past Searches in Local Storage
+function storeWord(term) {
+  var entry = {
+    word: term,
+  };
+
+  // Get Current Search List
+  var currentSearches = localStorage.getItem("searches");
+
+  // Validate/Parse Current List
+  if (!currentSearches) {
+    currentSearches = [];
+  } else {
+    currentSearches = JSON.parse(currentSearches);
+  }
+  // Append New Entry to List
+  currentSearches.push(entry);
+
+  // Set Updated List in Local Storage
+  localStorage.setItem("searches", JSON.stringify(currentSearches));
+}
+
+// Function to Validate and Get Synonyms
+function getSynonyms(slideDiv, synonymsArray) {
+  if (synonymsArray.length > 0) {
+    let synonymsEl = document.createElement("p");
+    let synonymsTitle = document.createElement("span");
+    let synonymsList = document.createElement("ul");
+
+    for (let k = 0; k < synonymsArray.length; k++) {
+      let synonyms = document.createElement("li");
+
+      synonyms.textContent = synonymsArray[k];
+
+      synonymsList.appendChild(synonyms);
+    }
+
+    synonymsTitle.textContent = "Synonyms:";
+    synonymsEl.classList.add("synonyms");
+    synonymsEl.appendChild(synonymsTitle);
+    synonymsEl.appendChild(synonymsList);
+    slideDiv.appendChild(synonymsEl);
+  }
+}
+
+// Function to Validate and Get Antonyms
+function getAntonyms(antonymsArray) {
+  if (antonymsArray.length > 0) {
+    let antonymsEl = document.createElement("p");
+    let antonymsTitle = document.createElement("span");
+    let antonymsList = document.createElement("ul");
+
+    for (let k = 0; k < antonymsArray.length; k++) {
+      let antonyms = document.createElement("li");
+
+      antonyms.textContent = antonymsArray[k];
+
+      antonymsList.appendChild(antonyms);
+    }
+
+    antonymsTitle.textContent = "Antonyms:";
+    antonymsEl.classList.add("antonyms");
+    antonymsEl.appendChild(antonymsTitle);
+    antonymsEl.appendChild(antonymsList);
+    slideDiv.appendChild(antonymsEl);
+  }
+}
+
+// Function to Validate and Get Example
+function getExample(slideDiv, definition) {
+  if (definition.example) {
+    let example = document.createElement("p");
+    example.textContent = `ex. ${definition.example}`;
+    example.classList.add("example");
+    slideDiv.appendChild(example);
+  }
+}
+
+// Display Definitions by Part of Speech
 function meanings(meaningsData) {
   for (let i = 0; i < meaningsData.length; i++) {
     let definitions = meaningsData[i].definitions;
@@ -30,70 +145,29 @@ function meanings(meaningsData) {
       slideDiv.appendChild(definition);
 
       // Validate and Get Example Sentence, if available
-      if (definitions[j].example) {
-        let example = document.createElement("p");
-        example.textContent = `ex. ${definitions[j].example}`;
-        example.classList.add("example");
-        slideDiv.appendChild(example);
-      }
+      getExample(slideDiv, definitions[j]);
 
       // Get Antonyms and Synonyms Arrays
       let antonymsArray = meaningsData[i].antonyms;
       let synonymsArray = meaningsData[i].synonyms;
 
-      // Validate and Get Antonyms, if available
-      if (antonymsArray.length > 0) {
-        let antonymsEl = document.createElement("p");
-        let antonymsTitle = document.createElement("span");
-        let antonymsList = document.createElement("ul");
-
-        for (let k = 0; k < antonymsArray.length; k++) {
-          let antonyms = document.createElement("li");
-
-          antonyms.textContent = antonymsArray[k];
-
-          antonymsList.appendChild(antonyms);
-        }
-
-        antonymsTitle.textContent = "Antonyms:";
-        antonymsEl.classList.add("antonyms");
-        antonymsEl.appendChild(antonymsTitle);
-        antonymsEl.appendChild(antonymsList);
-        slideDiv.appendChild(antonymsEl);
-      }
-
-      // Validate and Get Synonyms, if available
-      if (synonymsArray.length > 0) {
-        let synonymsEl = document.createElement("p");
-        let synonymsTitle = document.createElement("span");
-        let synonymsList = document.createElement("ul");
-
-        for (let k = 0; k < synonymsArray.length; k++) {
-          let synonyms = document.createElement("li");
-
-          synonyms.textContent = synonymsArray[k];
-
-          synonymsList.appendChild(synonyms);
-        }
-
-        synonymsTitle.textContent = "Synonyms:";
-        synonymsEl.classList.add("synonyms");
-        synonymsEl.appendChild(synonymsTitle);
-        synonymsEl.appendChild(synonymsList);
-        slideDiv.appendChild(synonymsEl);
-      }
+      // Validate and Get Antonyms & Synonyms, if available
+      getAntonyms(slideDiv, antonymsArray);
+      getSynonyms(slideDiv, synonymsArray);
 
       // Append Everything Created to Orbit Slides
       slide.appendChild(slideDiv);
+      console.log(slide);
       definitionCard.appendChild(slide);
     }
   }
 }
 
-// Function to Display Images
+// Display Images
 function displayImages(response) {
   let imagesCont = document.createElement("ul");
-  for (i = 0; i < 6; i++) {
+  let numOfImages = 6;
+  for (i = 0; i < numOfImages; i++) {
     let imagesEl = document.createElement("li");
     let imageEl = document.createElement("figure");
     let imageLink = document.createElement("a");
@@ -122,8 +196,7 @@ function imageProcessor(term) {
     .get(pexelURL, { headers: { Authorization: `Bearer ${pexelApiKey}` } })
     .then(displayImages)
     .catch((err) => {
-      console.log(err);
-      return "Not able to retrieve Images";
+      console.log("No Images Available");
     });
 }
 
@@ -142,11 +215,8 @@ function getEtymologies(term) {
     .get(merriamApiURL)
     .then(displayEtymology)
     .catch((err) => {
-      console.log(err);
-      return "Not able to retrieve Etymology Details";
+      console.log("No Entymology Available");
     });
-
-  return;
 }
 
 // Function to Display Phonetics
@@ -179,10 +249,20 @@ function displayDict(dictResponse) {
   wordTitle.classList.add("text-capitalize");
   mainCard.appendChild(wordTitle);
 
-  phonetics(dictResponse.data[0].phonetics);
-  getEtymologies(dictResponse.data[0].word);
-  imageProcessor(dictResponse.data[0].word);
-  meanings(dictResponse.data[0].meanings);
+  let responseData = dictResponse.data[0];
+  phonetics(responseData.phonetics);
+  getEtymologies(responseData.word);
+  imageProcessor(responseData.word);
+  meanings(responseData.meanings);
+}
+
+// Error Handling Function
+function dictErrorHandler(term, response) {
+  mainEl.classList.remove("hidden");
+
+  let errorMessage = document.createElement("h2");
+  errorMessage.textContent = `${response.data.message} - ${term}`;
+  mainCard.appendChild(errorMessage);
 }
 
 // Function to Process Dictionary API Request
@@ -191,13 +271,12 @@ function dictProcessor(term) {
   axios
     .get(dictApiURL)
     .then(displayDict)
-    .catch((err) => {
-      console.log(err);
-      return "Not able to retrieve Dictionary Details";
+    .catch((error) => {
+      if (error.response) {
+        dictErrorHandler(term, error.response);
+      }
     });
 }
-
-// Function to Get Basic Dictionary Details from Dictionary API Entry API
 
 // Function to Handle Search Request
 function searchHandler(event) {
@@ -216,6 +295,8 @@ function searchHandler(event) {
     alert("Word is required");
   } else {
     dictProcessor(word);
+    storeWord(word);
+    displaySearchHistory();
 
     // Reset Search Field
     wordInput.value = "";
@@ -224,23 +305,33 @@ function searchHandler(event) {
 
 // Function to Process Random API Response
 function randomProcessor(randomResponse) {
-  console.log(randomResponse.data[0]);
+  // Reset Main Section
+  mainCard.innerHTML = "";
+  definitionCard.innerHTML = "";
+
   dictProcessor(randomResponse.data[0]);
 }
 
 // Function to Handle Random Word Request
-// TO DO: Convert to a Set Interval so only ONE Word is generated per day
 function randomHandler() {
   let randomApiURL = "https://random-word-api.herokuapp.com/word?number=1";
   axios
     .get(randomApiURL)
     .then(randomProcessor)
     .catch((err) => {
-      console.log(err);
-      return "No Word Available";
+      console.log("No Word Available");
     });
+}
+
+// Reset History
+function resetHandler() {
+  localStorage.clear();
+  pastSearch.innerHTML = "";
+  pastSearch.classList.add("hidden");
 }
 
 // Event Listeners
 searchWord.addEventListener("submit", searchHandler);
 randomWord.addEventListener("click", randomHandler);
+pastSearch.addEventListener("click", pastSearchHandler);
+resetSearch.addEventListener("click", resetHandler);
